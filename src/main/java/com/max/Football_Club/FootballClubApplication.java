f73233f3715260de34609af7520c11dec5bfdba0
Maks <makzylinski@gmail.com>
2026-09-21 22:03:29 +0200

    Initial commit: Spring Boot project skeleton

---
+++ src/main/java/com/max/Football_Club/FootballClubApplication.java
@@ -0,0 +1,13 @@
+package com.max.Football_Club;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballClubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/max/Football_Club/FootballClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.max.Football_Club;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
